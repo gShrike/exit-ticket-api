e20112d38db30660b41e3db4db8aa7b2ba05dfa1
coloradokim <schlesinger.kim@gmail.com>
2017-09-18 10:48:12 -0600

    pruning bolierplate express app

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var questions = require('./routes/questions');
+var quizzes = require('./routes/quizzes');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +20,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('v1/api/questions', questions);
+app.use('v1/api/quizzes', quizzes);
+app.use('v1/api/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
