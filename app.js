0b7ee02ef2f14a9ce1bfa508626e12c9e3c01107
coloradokim <schlesinger.kim@gmail.com>
2017-09-18 11:30:36 -0600

    basic routes working

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
+
 var questions = require('./routes/questions');
 var quizzes = require('./routes/quizzes');
 var users = require('./routes/users');
-
-var app = express();
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,26 +19,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('v1/api/questions', questions);
-app.use('v1/api/quizzes', quizzes);
-app.use('v1/api/users', users);
+
+app.use('/v1/api/questions', questions);
+app.use('/v1/api/quizzes', quizzes);
+app.use('/v1/api/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
 });
 
 module.exports = app;
